9c9afe7b3ec857d31b517752c7255190d071c8e3
bakakoozin <cyrilpebre@gmail.com>
2023-07-31 18:18:16 +0200

    affichage de tout le pokedex

--- app.js
+++ app.js
@@ -5,20 +5,19 @@ let pokemons = require('./mock-pokemon');
 const app = express()
 const port = 3000
 
-app.get('/', (req,res) => res.send('Hello again, Express !'))
+app.get('/', (req, res) => res.send('Hello again, Express !'))
 
-app.get('/api/pokemons/:id', (req,res) => {
+app.get('api/pokemons', (req, res) => {
+    const message = 'La liste des pokémons a bien été récupérée.'
+    res.json(success(message, pokemons))
+})
+
+app.get('/api/pokemons/:id', (req, res) => {
     const id = parseInt(req.params.id)
     const pokemon = pokemons.find(pokemon => pokemon.id === id)
     const message = 'Un pokémon a bien été trouvé'
     res.json(success(message, pokemon))
 })
-
-app.get('/api/pokemons', (req,res) => {
-    res.send(`Il y a ${pokemons.length} pokémons dans le pokédex, pour le moment.`)
-})
-
-
 
 
 app.listen(port, () => console.log(`Notre application Node est démarrée sur: http://localhost:${port}`))
