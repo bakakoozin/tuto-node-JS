c501ecf09f23c97221866be74638d5f1097d6a8b
bakakoozin <cyrilpebre@gmail.com>
2023-07-31 19:44:19 +0200

    ajout de middlewares et de pokemons dans le tableau

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require('express')
-const { success } = require('./helper.js')
+const morgan = require('morgan')
+const favicon = require('serve-favicon')
+const { success, getUniqueId } = require('./helper.js')
 let pokemons = require('./mock-pokemon');
 
 const app = express()
 const port = 3000
 
+app
+    .use(favicon(__dirname + '/favicon.ico'))
+    .use(morgan('dev'))
+
 app.get('/', (req, res) => res.send('Hello again, Express !'))
 
-app.get('api/pokemons', (req, res) => {
+app.get('/api/pokemons', (req, res) => {
     const message = 'La liste des pokémons a bien été récupérée.'
     res.json(success(message, pokemons))
 })
@@ -17,6 +23,14 @@ app.get('/api/pokemons/:id', (req, res) => {
     const pokemon = pokemons.find(pokemon => pokemon.id === id)
     const message = 'Un pokémon a bien été trouvé'
     res.json(success(message, pokemon))
+})
+
+app.post('/api/pokemons', (req, res) => {
+    const id = getUniqueId(pokemons)
+    const pokemonCreated = { ...req.body, ...{id: id, created: new Date()}}
+    pokemons.push(pokemonCreated)
+    const message = `Le pokemon ${pokemonCreated.name} a bien été créé.`
+    res.json(success(message, pokemonCreated))
 })
 
 
